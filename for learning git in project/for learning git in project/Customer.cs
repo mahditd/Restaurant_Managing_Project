1fb57eece64eb9e4a8fa71d600f769f09dceb370
Sina Fallahi <sinafallahi005@gmail.com>
2024-06-07 18:08:08 +0330

    added Customer class to develop brach

---
+++ for learning git in project/for learning git in project/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_learning_git_in_project
+{
+    class Customer
+    {
+        string password;
+    }
+}
