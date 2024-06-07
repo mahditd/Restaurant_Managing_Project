c4cb6c7542a1e03407886e934d2b11bf58750d88
Sina Fallahi <sinafallahi005@gmail.com>
2024-06-07 18:39:27 +0330

    added Complaint and Comment classes and Cusomer was written

--- for learning git in project/for learning git in project/Customer.cs
+++ for learning git in project/for learning git in project/Customer.cs
@@ -6,9 +6,28 @@ using System.Threading.Tasks;
 
 namespace for_learning_git_in_project
 {
+    enum Gender
+    {
+        Male,
+        Female
+    }
+    enum Subscription
+    {
+        Gold,
+        Silver,
+        Bronze
+    }
     class Customer
     {
+        string userName;
         string password;
         string name;
+        string phone;
+        List<Food> foods = new List<Food>();
+        List<Order> orders = new List<Order>();
+        string postalCode;
+        Gender Gender;
+        List<Comment> comments = new List<Comment>();
+        List<Complaint> complaints = new List<Complaint>();
     }
 }
